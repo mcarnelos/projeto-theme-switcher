d09a95a6111db5db4762a98e199cedd7ef91c516
Murilo Carnelos <muriloc.c@hotmail.com>
2022-04-30 13:11:10 -0300

    alteracao nas funcoes do js

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 function troca_cor_light() {
-    let img = window.document.getElementById('light') //pega a imagem
 
-    img.src = './assets/moon.svg'
-    
-    document.body.style.backgroundColor = '#292C35'
+    const element = document.querySelector('body')
+
+    element.classList.add('light', 'dark')
+    console.log(element.classList)
+    element.classList.toggle('light')
 }
 
 function troca_cor_dark() {
-    let img = window.document.getElementById('dark') //pega a imagem
 
-    img.src = './assets/sun.svg'
-    
-    document.body.style.backgroundColor = '#F1F1F1'
+    const element = document.querySelector('body')
+
+    element.classList.add('light', 'dark')
+    console.log(element.classList)
+    element.classList.toggle('dark')
 }
